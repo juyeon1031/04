7968492a59838a6e95415526aadd166916b53f8c
jooyeon-1031 <jmhjoin@naver.com>
2022-09-27 17:04:19 +0900

    Revert "Update main.c"
    This reverts commit 930b3f53c4bfbcb719f34e30f7341b470c0ddb84.

--- main.c
+++ main.c
@@ -4,13 +4,10 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x;
-	printf("input the year : ");
-	scanf("%i", &x);
+	int sec;
+	printf("input the second:");
+	scanf("%i", &sec);
 	
-	if((x%4==0 && x%100!=0)||x%400==0)
-		printf("%i is leap year!\n", x);
-	else
-		printf("%i is not leap year!\n", x);
+	printf("the time is %i : %i", sec/60, sec%60);
 	return 0;
 }
